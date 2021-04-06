5823ba0d410375cbb6b650ed30f71cb5dcd20074
anniep8911 <pjatopazz@gmail.com>
2021-04-06 10:00:08 +0900

    5th commit

--- js/day82-1_script.js
+++ js/day82-1_script.js
@@ -5,7 +5,10 @@ $(function(){
 
     //이벤트를 'on'(연결)하겠다는 의미. >> on이 편한점은 여러 동작을
     //한꺼번에 연결할 수 있음
-    $('.btTop').on('click',function(){
-        
+    //들어가는 변수는 event라는 의미, evt e만 써도 됨.
+    //event.type은 해당 이벤트 발생시 타입을 보여줌
+    $('.btTop').on('click mouseenter mouseleave',function(evt){
+        console.log(evt.type);
     });
+    
 });
