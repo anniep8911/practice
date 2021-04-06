43ae91afa9c5c1b4bb77b2503e4ebbaa564f025f
anniep8911 <pjatopazz@gmail.com>
2021-04-06 12:57:10 +0900

    commit 9th

---
+++ js/day82-2_script_renw.js
@@ -0,0 +1,20 @@
+$(function(){
+    var offset_ct=0;
+    var i=0;
+    //휠다운시 nextPage, 휠업 prevPage
+    $('.btNext').on('click',nextPage);
+    $('.btPrev').on('click',prevPage);
+
+    function nextPage(){
+        offset_ct = $('.content').eq(i).offset().top;
+        //각 content의 offset().top의 값을 html의 scrollTop의 값으로 대입
+        $('html').animate({scrollTop:offset_ct},1000);
+        i = (i+1)%4;
+    };
+    function prevPage(){
+        offset_ct = $('.content').eq(i).offset().top;
+        //각 content의 offset().top의 값을 html의 scrollTop의 값으로 대입
+        $('html').animate({scrollTop:offset_ct},1000);
+        i = (i+3)%4;
+    }
+});
