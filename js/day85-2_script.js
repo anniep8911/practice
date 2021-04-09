bdd7078d63f58349adb5ed6de9ef125dfd6048e3
anniep8911 <pjatopazz@gmail.com>
2021-04-09 13:08:04 +0900

    commit

--- js/day85-2_script.js
+++ js/day85-2_script.js
@@ -24,4 +24,17 @@ $(function(){
             containment:'parent',
             axis:'x'
         });
+
+        $bar.on('drag',drgaHandler);
+
+        // .bar의 값이 0 > artG의 marginLeft :0 , .bar값:770px > .artG값은 -1350px이 됨 
+        var barPos=0, agPos=0;
+        function drgaHandler(){
+            barPos=$bar.css('left');
+            barPos=parseInt(barPos);
+            //현재 커서위치를 전체 움직일 크기로 빼고, 현재 커서가 움직일수있는 최대값으로 나누면 됨
+            agPos= barPos * -1350 / 770;
+            $artG.css({'marginLeft':agPos});
+            console.log(barPos+','+agPos);
+        }
 });
