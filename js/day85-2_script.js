34e83961d41398af420d1886135c0b73af006015
anniep8911 <pjatopazz@gmail.com>
2021-04-09 12:41:44 +0900

    commit

--- js/day85-2_script.js
+++ js/day85-2_script.js
@@ -1,13 +1,12 @@
 $(function(){
     var $ct32=$('.ct32'),
         $artG=$ct32.find('.artGroup'),
-        $art=$ct32.find('article');
+        $art=$ct32.find('article'),
+        $bar = $ct32.find('.bar');
 
         for(var i=1;i<8;i++){
             $art.clone().appendTo($artG);
         }
-        
-        
         var j= $artG.children('article:last').index()+1;
         //크기키우기
         $artG.outerWidth(270*j);
@@ -15,7 +14,14 @@ $(function(){
         //현재 article의 값을 반영해줌
         $art=$artG.find('article');
         //E.each(fn(){}) 선택된 요소 E의 각각이라는 의미// 각각fn실행
-        $art.each(function(index){
-            $art.find('.image').addClass('i'+index);
+        $art.each(function(k){
+            $art.eq(k).find('.image').addClass('i'+(k+1));
+            $('.i'+(k+1)).css({backgroundImage:'url(img/pic0'+(k+1)+'.jpg)'});
+        });
+
+        //드래그바 동작
+        $bar.draggable({
+            containment:'parent',
+            axis:'x'
         });
 });
