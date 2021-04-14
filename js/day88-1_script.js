c54479c841b3b1ee2fadb06c6529a03e2c9a1e76
anniep8911 <pjatopazz@gmail.com>
2021-04-14 17:32:21 +0900

    commit

--- js/day88-1_script.js
+++ js/day88-1_script.js
@@ -5,6 +5,8 @@ $(function(){
         boxHide = {width:0,height:0,borderRadius:'160px'},
         boxShow = {width:'160px',height:'160px',borderRadius:0},
         $h4 = $dropBox.find('h4'),
+        $h5 = $dropBox.find('h5'),
+        $p = $dropBox.find('p'),
         txt='',
         $stop = $dropBox.find('.btStop'),
         $myMusic = $dropBox.find('.texts audio'),
@@ -27,6 +29,10 @@ $(function(){
                 $('.dragging').stop().animate(boxHide,function(){
                     txt= $(this).find('h4').text();
                     $h4.text(txt);
+                    txt=$(this).find('h5').text();
+                    $h5.text(txt);
+                    txt=$(this).find('p').text();
+                    $p.text(txt);
                     $dropBox.find('.album').addClass('run');
                     $image =$(this).css('background-image');
                     $dropBox.find('.album .core').css('background-image',$image);
@@ -40,7 +46,9 @@ $(function(){
             $dropBox.find('.album').removeClass('run');
             $dragBox.removeClass('dragging');
             $dragBox.animate(boxShow);
-            $h4.text('drag here!');
+            $h4.text('Drop here');
+            $h5.text('앨범이미지를 여기에 드래그 해주세요.');
+            $p.text(' 여기에 앨범 설명이 보입니다.');
             $dropBox.find('.album .core').css('background-image','');
             $myMusic.attr('src','');
         });
